c034a09e0bff132d651ea54cdff65356e05b3d34
Jeonyujin77 <wjsdbwls7777@gmail.com>
2022-12-16 20:46:10 +0900

    [feature] css 수정

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,13 @@ import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./redux/store";
 import "./index.css";
+import Layout from "./components/layout/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </Provider>
 );
